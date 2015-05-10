230a693f85d3a0a32ae0efd6a7271e875dc32c1c
James B Kemp <jamesbkemp@me.com>
2015-05-10 20:19:41 +0100

    adding secrets.yml back

---
+++ spec/features/user_registration_feature_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+# describe 'user registration' do
+#   it 'confirms user registration' do
+#     visit '/users/index'
+#     fill_in 'Email', with: 'test1@test.com'
+#     expect(page).to have_content("Congratulations")
+#   end
+# end
