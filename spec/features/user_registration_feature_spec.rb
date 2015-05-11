b9a045686b1f9d050a5c933a9ca4e6b92b5618df
James B Kemp <jamesbkemp@me.com>
2015-05-11 21:03:41 +0100

    adding name field, need to aligh with css responsiev columns properly

--- spec/features/user_registration_feature_spec.rb
+++ spec/features/user_registration_feature_spec.rb
@@ -3,7 +3,8 @@ require 'rails_helper'
 describe 'user registration' do
   it 'confirms user registration' do
     visit '/users/index'
-    fill_in 'Email', with: 'test1@test.com'
+    fill_in 'Your email here...', with: 'test1@test.com'
+    click_button('Notify me')
     expect(page).to have_content("Congratulations")
   end
 end
